7968e243292824e8bedc44fffd31e0ca01172848
Vaibhav Srivastava <vabnix@hotmail.com>
2025-01-31 10:33:57 -0600

    updating readme

--- src/main/java/com/vaibhav/spring/repositories/UserRepository.java
+++ src/main/java/com/vaibhav/spring/repositories/UserRepository.java
@@ -4,8 +4,6 @@ import com.vaibhav.spring.data.UserData;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface UserRepository extends MongoRepository<UserData, String> {
 
